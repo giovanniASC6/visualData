da06ca6569402b17f60fc216f02affa38f5fc9ce
giovanniASC6 <giovanni.owens@gmail.com>
2019-08-02 12:08:57 -0400

    visualData

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-const first = list[0];
+// const first = list[0];
 const main= document.querySelector(`main`)
 
 
-const createCard = obj=>{
+const createCard = (obj, indx)=>{
     const newCard= document.createElement("div")
     newCard.className= "card"
+    newCard.id= indx;
 
     const face = document.createElement("img");
     face.src= obj.picture.medium
@@ -16,4 +17,12 @@ main.appendChild(newCard);
 
 };
 
-createCard(first);
+// createCard(first);
+
+const loopCreate= array=>{
+    for(let i=0; i<array.length; i++){
+        createCard(array[i], i) }
+    
+}
+
+loopCreate(list)
